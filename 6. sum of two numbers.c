37dfb4afa06a8f18481f729b7e76dbb76aa62851
ritikgupta-cs <ritik.guptacse2024@indoreinstitute.com>
2025-08-26 21:41:17 +0530

    Add files via upload

---
+++ 6. sum of two numbers.c
@@ -0,0 +1,10 @@
+//find sum of two numbers
+#include <stdio.h>
+int main()
+{
+	int x,y;
+	printf("ENTER TWO NUMBERS: ");
+    scanf("%d%d",&x,&y);
+    printf("sum of two integers is=%d",x+y);
+    return 0;
+}
